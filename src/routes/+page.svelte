84b614be92901b9643073b5d46f72641ff95048e
Prateeksha Singh <pratu16x7@gmail.com>
2024-08-03 10:50:35 -0400

    bring project into Obsidian Dropbox vault, put node modules up the tree outside the vault, let vite know that directory, will need to update node modules the hard way but whatever, add a counter elem to ensure stuff works. Next up, read git logs inObsidian too

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+	import Counter from './Counter.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	try editing <strong>src/routes/+page.svelte</strong>
+</p>
+
+<Counter />
