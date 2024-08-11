ba302f84f1ce9d1bf6db20fd5de79e6dcd0808ba
Prateeksha Singh <pratu16x7@gmail.com>
2024-08-11 17:48:57 -0400

    codemirror: install and use codemirror like chatgpt showed in first svelte repo

--- src/lib/FunctionPreview.svelte
+++ src/lib/FunctionPreview.svelte
@@ -1,15 +1,50 @@
-<script lang="ts">
-  export let program: string;
+<script>
+  import { onMount } from 'svelte';
+  import { EditorView, basicSetup } from 'codemirror';
+  import { javascript } from '@codemirror/lang-javascript';
+
+  export let program;
+
+  let editorContainer;
+
+  onMount(() => {
+    let editor = new EditorView({
+      doc: program,
+      extensions: [basicSetup, javascript()],
+      parent: editorContainer,
+      readOnly: true
+    });
+
+    editor.dispatch({
+      changes: { from: 0, to: editor.state.doc.length, insert: program }
+    });
+
+    editor.updateListener = (update) => {
+      if (update.changes) {
+        program = update.state.doc.toString();
+      }
+    };
+  });
+
+  $: if (editorContainer) {
+    editorContainer.innerHTML = '';
+    let editor = new EditorView({
+      doc: program,
+      extensions: [basicSetup, javascript()],
+      parent: editorContainer
+    });
+  }
 </script>
 
-<div>
-  <!-- wow bind works child to parent ... (go to +page.svelte) -->
-  <textarea bind:value={program} />
-</div>
+<div bind:this={editorContainer} class="editor"></div>
 
 <style>
-  textarea {
-    width: 300px;
-    height: 150px;
+  .editor {
+    height: 100%;
+    width: 100%;
+    border: 1px solid #ddd;
   }
+  /* .CodeMirror {
+    height: 100%;
+  } */
 </style>
