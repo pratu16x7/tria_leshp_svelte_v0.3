6419f2033c8a711f629935b8bc3ffd328144cafa
Prateeksha Singh <pratu16x7@gmail.com>
2024-08-16 18:23:53 -0400

    text highlighting works in codemirror finally! Let's see if this is any help with the AST pointers

--- _scraps.ts
+++ _scraps.ts
@@ -1,3 +1,135 @@
+
+// explanation for highliting https://stackoverflow.com/a/72793520/6495043
+// forum example I copied: https://discuss.codemirror.net/t/codemirror-6-highlighting-specific-substring/6615
+const highlight_effect = StateEffect.define<Range<Decoration>[]>();
+
+const highlight_extension = StateField.define({
+  create() { return Decoration.none },
+  update(value, transaction) {
+	value = value.map(transaction.changes)
+
+	for (let effect of transaction.effects) {
+	  if (effect.is(highlight_effect)) value = value.update({add: effect.value, sort: true})
+	}
+
+	return value
+  },
+  provide: f => EditorView.decorations.from(f)
+});
+
+let state = EditorState.create({/* ... */ extensions: [highlight_extension]});
+
+let view = new EditorView({
+  state,
+  parent: element
+});
+
+
+let cursor = new SearchCursor(view.state.doc, "# COMPLETE THIS");
+
+cursor.next();
+
+const highlight_decoration = Decoration.mark({
+  attributes: {style: "background-color: yellow"}
+});
+
+view.dispatch({
+  effects: highlight_effect.of([highlight_decoration.range(cursor.value.from, cursor.value.to)])
+});
+
+
+
+
+
+
+const highlightDeco = Decoration.mark({ class: 'highlight' });
+
+let builder = new RangeSetBuilder();
+builder.add(from, to, highlightDeco);
+
+let decoSet = builder.finish();
+
+console.log(editor);
+
+editor?.dispatch({
+  effects: EditorView.decorations.of(decoSet)
+});
+
+
+
+
+<script lang="ts">
+  export let program: string;
+</script>
+
+<div>
+  <!-- wow bind works child to parent ... (go to +page.svelte) -->
+  <textarea bind:value={program} />
+</div>
+
+<style>
+  textarea {
+    width: 300px;
+    height: 150px;
+  }
+</style>
+
+
+
+
+
+<script>
+  import { onMount } from 'svelte';
+  import { EditorView, basicSetup } from 'codemirror';
+  import { javascript } from '@codemirror/lang-javascript';
+
+  export let program;
+
+  let editorContainer;
+
+  onMount(() => {
+    let editor = new EditorView({
+      doc: program,
+      extensions: [basicSetup, javascript()],
+      parent: editorContainer,
+      readOnly: true
+    });
+
+    editor.dispatch({
+      changes: { from: 0, to: editor.state.doc.length, insert: program }
+    });
+
+    editor.updateListener = (update) => {
+      if (update.changes) {
+        program = update.state.doc.toString();
+      }
+    };
+  });
+
+  $: if (editorContainer) {
+    editorContainer.innerHTML = '';
+    let editor = new EditorView({
+      doc: program,
+      extensions: [basicSetup, javascript()],
+      parent: editorContainer
+    });
+  }
+</script>
+
+<div bind:this={editorContainer} class="editor"></div>
+
+<style>
+  .editor {
+    height: 100%;
+    width: 100%;
+    border: 1px solid #ddd;
+  }
+  /* .CodeMirror {
+    height: 100%;
+  } */
+</style>
+
+
 // <!--https://github.com/touchifyapp/svelte-codemirror-editor-->
 // <script lang="ts">
 //   import CodeMirror from 'svelte-codemirror-editor';
--- src/lib/FunctionPreview.svelte
+++ src/lib/FunctionPreview.svelte
@@ -1,42 +1,95 @@
-<script>
+<script lang="ts">
   import { onMount } from 'svelte';
   import { EditorView, basicSetup } from 'codemirror';
   import { javascript } from '@codemirror/lang-javascript';
+  import { EditorState, StateField, StateEffect, Range } from '@codemirror/state';
+  import { SearchCursor } from '@codemirror/search';
+  import { Decoration } from '@codemirror/view';
 
-  export let program;
+  export let program: string;
 
-  let editorContainer;
+  let editorContainer: Element;
+  const highlight_effect = StateEffect.define<Range<Decoration>[]>();
+
+  const highlight_extension = StateField.define({
+    create() {
+      return Decoration.none;
+    },
+    update(value, transaction) {
+      value = value.map(transaction.changes);
+
+      for (let effect of transaction.effects) {
+        if (effect.is(highlight_effect)) value = value.update({ add: effect.value, sort: true });
+      }
+
+      return value;
+    },
+    provide: (f) => EditorView.decorations.from(f)
+  });
+
+  const highlight_decoration = Decoration.mark({
+    attributes: { style: 'background-color: yellow' }
+  });
 
   onMount(() => {
-    let editor = new EditorView({
+    let state = EditorState.create({ /* ... */ extensions: [highlight_extension] });
+
+    let editorView = new EditorView({
+      state,
       doc: program,
-      extensions: [basicSetup, javascript()],
-      parent: editorContainer,
-      readOnly: true
+      // extensions: [basicSetup, javascript(), highlight_extension],
+      extensions: [highlight_extension],
+      parent: editorContainer
     });
 
-    editor.dispatch({
-      changes: { from: 0, to: editor.state.doc.length, insert: program }
-    });
+    let cursor = new SearchCursor(editorView.state.doc, 'push');
 
-    editor.updateListener = (update) => {
-      if (update.changes) {
-        program = update.state.doc.toString();
-      }
-    };
+    cursor.next();
+
+    console.log(program, cursor.value);
+
+    // editorView.updateListener = (update) => {
+    //   if (update.changes) {
+    //     program = update.state.doc.toString();
+    //   }
+    // };
   });
 
   $: if (editorContainer) {
     editorContainer.innerHTML = '';
-    let editor = new EditorView({
+    let editorView = new EditorView({
       doc: program,
-      extensions: [basicSetup, javascript()],
+      extensions: [basicSetup, javascript(), highlight_extension],
+      // extensions: [],
       parent: editorContainer
+    });
+
+    editorView.dispatch({
+      changes: { from: 0, to: editorView.state.doc.length, insert: program },
+      // effects: highlight_effect.of([highlight_decoration.range(cursor.value.from, cursor.value.to)])
+      effects: highlight_effect.of([highlight_decoration.range(10, 20)])
     });
   }
 </script>
 
+<p>okay not sure anymore how to bind, but we don't need it yet</p>
 <div bind:this={editorContainer} class="editor"></div>
+
+<!--<script lang="ts">
+  export let program: string;
+</script>
+
+<div>
+  <p> wow bind works child to parent ... (go to +page.svelte) </p>
+  <textarea bind:value={program} />
+</div>
+
+<style>
+  textarea {
+    width: 300px;
+    height: 150px;
+  }
+</style> -->
 
 <style>
   .editor {
