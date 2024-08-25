9689cfe5fc6d712e93242a47307c45c3b9c458e4
Prateeksha Singh <pratu16x7@gmail.com>
2024-08-25 00:07:51 -0400

    add some pan and zoom :)

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -5,12 +5,50 @@
 
   let canvas;
   let { nodes, edges } = jsonData;
+  let isDragging = false;
+  let lastPosX, lastPosY;
 
   onMount(() => {
     canvas = new Canvas('graph-canvas', {
       width: 1900,
       height: 1000,
-      backgroundColor: '#f0f0f0'
+      backgroundColor: '#f0f0f0',
+      selection: false
+    });
+
+    canvas.forEachObject((obj) => (obj.selectable = false));
+
+    canvas.on('mouse:down', (opt) => {
+      var evt = opt.e;
+      if (evt.altKey === true) {
+        isDragging = true;
+        lastPosX = evt.clientX;
+        lastPosY = evt.clientY;
+      }
+    });
+
+    canvas.on('mouse:move', (opt) => {
+      if (isDragging) {
+        var e = opt.e;
+        canvas.relativePan({ x: e.clientX - lastPosX, y: e.clientY - lastPosY });
+        lastPosX = e.clientX;
+        lastPosY = e.clientY;
+      }
+    });
+
+    canvas.on('mouse:up', () => {
+      isDragging = false;
+    });
+
+    canvas.on('mouse:wheel', (opt) => {
+      var delta = opt.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
     });
 
     nodes.forEach((node) => {
@@ -29,15 +67,10 @@
     const text = new IText(node.text, {
       left: node.x,
       top: node.y,
-      width: node.width,
       fill: 'black',
       fontSize: 14,
       selectable: false
     });
-
-    // Optional: Adjust text style if necessary
-    text.set({ width: node.width, height: node.height });
-
     canvas.add(text);
   }
 
@@ -49,7 +82,6 @@
       height: node.height,
       fill: '#999'
     });
-
     const text = new IText('Link to Video', {
       left: node.x,
       top: node.y + node.height / 2,
@@ -58,20 +90,16 @@
       originY: 'center',
       fill: 'white'
     });
-
     const group = new Group([rect, text], {
       hasControls: false,
       hasBorders: false,
       selectable: true,
       subTargetCheck: true,
-      url: node.url // Store the URL in the group for easy access
+      url: node.url
     });
-
-    // Add an event listener for clicks
     group.on('mousedown', function () {
-      window.open(this.url); // Open the link in a new tab
+      window.open(this.url);
     });
-
     canvas.add(group);
   }
 </script>
