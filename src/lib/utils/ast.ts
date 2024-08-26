28f892b522c04124d16e2b91b65f7e9c522430e1
Prateeksha Singh <pratu16x7@gmail.com>
2024-08-26 00:24:22 -0400

    finally, return the entire spoolItem and _res as part of it

--- src/lib/utils/ast.ts
+++ src/lib/utils/ast.ts
@@ -54,8 +54,6 @@ export function unspoolExecute(ast, program) {
     let context = prevContext ? clearPlayerPlayingState(prevContext) : {}; // by reference, hence change reflect in object too
     modeBlocks = structuredClone(modeBlocks);
 
-    let _res;
-
     let spoolItem = {
       _id: getRandomId(),
       nodeType,
@@ -63,7 +61,8 @@ export function unspoolExecute(ast, program) {
       context,
       modeBlocks,
       cursor,
-      levels
+      levels,
+      _res: undefined
     };
 
     if (astNodeTypesMeta[nodeType].spoolPush === 'before') {
@@ -72,21 +71,23 @@ export function unspoolExecute(ast, program) {
 
     switch (nodeType) {
       case 'Literal':
-        _res = node.value;
+        spoolItem._res = node.value;
         break;
 
       case 'Identifier':
         context[node.name]['isPlaying'] = true; // participating player
-        _res = context[node.name]['value']; // no eval needed, just context
+        spoolItem._res = context[node.name]['value']; // no eval needed, just context
         break;
 
       case 'ExpressionStatement':
-        _res = evaluate(node.expression, execLevel, modeBlocks); // come in last like a good person (eg. VariableDeclaration)
+        spoolItem._res = evaluate(node.expression, execLevel, modeBlocks)._res; // come in last like a good person (eg. VariableDeclaration)
         prevContext = structuredClone(context);
         break;
 
       case 'ArrayExpression':
-        _res = node.elements.map((element) => evaluate(element, execLevel, modeBlocks));
+        spoolItem._res = node.elements.map(
+          (element) => evaluate(element, execLevel, modeBlocks)._res
+        );
         break;
 
       case 'BlockStatement':
@@ -97,7 +98,7 @@ export function unspoolExecute(ast, program) {
         node.declarations.map((declaration) => {
           let varName = declaration.id.name;
           context[varName] = {
-            value: evaluate(declaration.init, execLevel, modeBlocks),
+            value: evaluate(declaration.init, execLevel, modeBlocks)._res,
             isPlaying: true // persists, working
           };
         });
@@ -107,11 +108,11 @@ export function unspoolExecute(ast, program) {
       case 'UnaryExpression':
         // TODO: copy what Binary does
         // TODO: what if this is not the final test
-        const arg = evaluate(node.argument, execLevel, modeBlocks);
+        const arg = evaluate(node.argument, execLevel, modeBlocks)._res;
 
         switch (node.operator) {
           case '!':
-            _res = !arg;
+            spoolItem._res = !arg;
           default:
             throw new Error('Unsupported unary operator: ' + node.operator);
         }
@@ -131,17 +132,17 @@ export function unspoolExecute(ast, program) {
           spoolItem.levels.anim = true;
         }
 
-        const left = evaluate(node.left, execLevel, modeBlocks);
-        const right = evaluate(node.right, execLevel, modeBlocks);
+        const left = evaluate(node.left, execLevel, modeBlocks)._res;
+        const right = evaluate(node.right, execLevel, modeBlocks)._res;
 
-        _res = binaryOperatorMap[node.operator](left, right);
+        spoolItem._res = binaryOperatorMap[node.operator](left, right);
         break;
 
       case 'AssignmentExpression':
         let varName = node.left.name;
 
-        const leftValue = evaluate(node.left, execLevel, modeBlocks);
-        const rightValue = evaluate(node.right, execLevel, modeBlocks);
+        const leftValue = evaluate(node.left, execLevel, modeBlocks)._res;
+        const rightValue = evaluate(node.right, execLevel, modeBlocks)._res;
 
         context[varName]['value'] = assignmentOperatorMap[node.operator](leftValue, rightValue);
         context[varName]['isPlaying'] = true; // active (updated) player
@@ -168,7 +169,7 @@ export function unspoolExecute(ast, program) {
         function ifTest() {
           let ifTestEval = evaluate(node.test, execLevel, modeBlocks, {
             parent: 'IfStatement'
-          });
+          })._res;
           if (linearSpool.length) {
             modeBlocks = linearSpool[linearSpool.length - 1]['modeBlocks'];
           }
@@ -189,7 +190,7 @@ export function unspoolExecute(ast, program) {
         function whileTest() {
           let testEval = evaluate(node.test, execLevel, modeBlocks, {
             parent: 'WhileStatement'
-          });
+          })._res;
 
           if (linearSpool.length) {
             modeBlocks = linearSpool[linearSpool.length - 1]['modeBlocks'];
@@ -205,7 +206,7 @@ export function unspoolExecute(ast, program) {
 
       case 'CallExpression':
         const callee = node.callee;
-        const args = node.arguments.map((arg) => evaluate(arg, execLevel, modeBlocks));
+        const args = node.arguments.map((arg) => evaluate(arg, execLevel, modeBlocks)._res);
 
         if (callee.type === 'MemberExpression') {
           // get console.log() out of the way
@@ -214,11 +215,12 @@ export function unspoolExecute(ast, program) {
             break;
           }
 
-          const object = evaluate(callee.object, execLevel, modeBlocks);
+          const evalalala = evaluate(callee.object, execLevel, modeBlocks);
+          const object = evalalala._res;
           const property = callee.property.name;
 
           if (typeof object[property] === 'function') {
-            _res = object[property](...args);
+            spoolItem._res = object[property](...args);
           } else {
             throw new Error('Unsupported method: ' + property);
           }
@@ -226,11 +228,11 @@ export function unspoolExecute(ast, program) {
         break;
 
       case 'MemberExpression':
-        const object = evaluate(node.object, execLevel, modeBlocks);
+        const object = evaluate(node.object, execLevel, modeBlocks)._res;
         const property = node.computed
-          ? evaluate(node.property, execLevel, modeBlocks)
+          ? evaluate(node.property, execLevel, modeBlocks)._res
           : node.property.name;
-        _res = object[property];
+        spoolItem._res = object[property];
         break;
 
       default:
@@ -242,7 +244,7 @@ export function unspoolExecute(ast, program) {
       linearSpool.push(spoolItem);
     }
 
-    return _res;
+    return spoolItem;
   }
 
   ast.body.map((node) => evaluate(node));
